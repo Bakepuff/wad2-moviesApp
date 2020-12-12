775636b562e5f4ce3b803d5073aec61185a519b6
Tan Shi <20091610@mail.wit.ie>
2020-12-12 20:52:44 +0000

    fix previous test bugs because of favorite page becomes private

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,6 +2,19 @@ let movies;
 const movieId = 497582; // Enola Holmes movie id
 let reviews;
 
+const USER = 'Tanshi'
+const PWD = '727399st'
+
+const login = () => {
+    cy.visit("/");
+    cy.get("nav").find("li").eq(3).find("a").click();
+    cy.get('.login-link').click()
+    cy.get('.user').children('input').clear().type(USER)
+    cy.get('.pwd').children('input').clear().type(PWD)
+    cy.get('.btn').click()
+    cy.wait(500)
+}
+
 describe("Navigation", () => {
   before(() => {
     cy.request(
@@ -29,30 +42,27 @@ describe("Navigation", () => {
     beforeEach(() => {
       cy.visit("/");
     });
-    // it("should navigate to the movie details page and change browser URL", () => {
-    //   cy.get(".card").eq(1).find("img").click();
-    //   cy.url().should("include", `/movies/${movies[1].id}`);
-    //   cy.get("h2").contains(movies[1].title);
-    // });
-    // it("should allow navigation from site header", () => {
-    //   cy.get("nav").find("li").eq(2).find("a").click();
-    //   cy.url().should("include", `/favorites`);
-    //   cy.get("h2").contains("Favorite Movies");
-    //   cy.get("nav").find("li").eq(1).find("a").click();
-    //   cy.url().should("not.include", `/favorites`);
-    //   cy.get("h2").contains("Discover Movies");
-    //   cy.get("nav").find("li").eq(2).find("a").click();
-    //   cy.get("nav.navbar-brand").find("a").click();
-    //   cy.url().should("not.include", `/favorites`);
-    //   cy.get("h2").contains("No. Movies");
-    // });
+    it("should navigate to the movie details page and change browser URL", () => {
+      cy.get(".card").eq(1).find("img").click();
+      cy.url().should("include", `/movies/${movies[1].id}`);
+      cy.get("h2").contains(movies[1].title);
+    });
+    it("should allow navigation from site header", () => {
+      login()
+      cy.get("nav").find("li").eq(3).find("a").click();
+      cy.get("h2").contains("Favorite Movies");
+      cy.get("nav").find("li").eq(1).find("a").click();
+      cy.url().should("not.include", `/favorites`);
+      cy.get("h2").contains("Discover Movies");
+      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.get("nav.navbar-brand").find("a").click();
+      cy.url().should("not.include", `/favorites`);
+      cy.get("h2").contains("No. Movies");
+    });
   });
-  /*describe("From the Movie Details page ", () => {
+  describe("From the Movie Details page ", () => {
     beforeEach(() => {
       cy.visit(`/movies/${movieId}`);
-    
-      
-     
     });
     it("should change browser URL when show/hide reviews is clicked", () => {
       cy.contains("Show Reviews").click();
@@ -68,19 +78,14 @@ describe("Navigation", () => {
       .children()
       .contains("Review By: ");
     });
-  });*/
-  // describe("From the Favorites page", () => {
-  //   beforeEach(() => {
-  //     cy.visit("/");
-  //     cy.get(".card").eq(0).find("button").click();
-  //     cy.get("nav").find("li").eq(2).find("a").click();
-  //   });
-  //   it("should navigate to the movies detail page and change the browser URL", () => {
-  //     cy.get(".card").eq(0).find("img").click();
-  //     cy.url().should("include", `/movies/${movies[0].id}`);
-  //     cy.get("h2").contains(movies[0].title);
-  //   });
-  // });
+  });
+  describe("From the Favorites page", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get(".card").eq(0).find("button").click();
+      cy.get("nav").find("li").eq(2).find("a").click();
+    });
+  });
   describe("The Go Back button", () => {
     beforeEach(() => {
       cy.visit("/");
